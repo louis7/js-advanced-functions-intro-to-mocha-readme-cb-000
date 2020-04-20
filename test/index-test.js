7ccde46802eabc2c1af2fbd995e50effedbe5189
louis baptiste <louisvis@ymail.com>
2020-04-20 01:37:06 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-sdescribe('Intro to Mocha', function () {
+describe('Intro to Mocha', function () {
   describe('theTruth()', function () {
     it('should return true', function () {
       expect(theTruth()).toEqual(true);
